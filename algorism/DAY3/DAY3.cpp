c85f943650a9ff0dcaaedb10d64b9bace4006ba9
vegemil <euna501@gmail.com>
2015-01-22 23:15:13 +0900

    같은 문자가 연속으로 반복될 경우, 그 횟수를 사용해 문자열을 압축하는 메서드를 구현하라. 가령 압축해야 할 문자열이 aabccccccccaaa라면 a2b1c8a3과 같이 압축되어야 한다. 압축결과로 만들어지는 문자열이 원래 문자열 보다 짧아지지 않는 경우, 이 메서드는 원래 문자열을 그대로 반환해야 한다.

---
+++ algorism/DAY3/DAY3.cpp
@@ -0,0 +1,100 @@
+// DAY3.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int count;
+
+	cin >> count;
+
+	string* word = new string[count];
+	string shortword;
+
+	int  temp = 0;
+	int startindex = 0;
+	int endindex = 0;
+	int k = 0;
+
+
+	for (int i = 0; i < count; ++i)
+	{
+		cin >> word[i];
+	}
+
+	for (int i = 0; i < count; ++i)
+	{
+		cout << "#testcase" << i + 1 << endl;
+
+		cout << word[i] << " " << word[i].length() << endl;
+
+		string::iterator  iter = word[i].begin();
+
+		while (k <= word[i].length())
+		{
+
+			 if ((iter + 1) == word[i].end())
+			{
+				if (*iter == *(iter - 1))
+					shortword += to_string(temp + 1);
+				else
+				{
+					shortword += *iter;
+					shortword += "1";
+				}
+				k = word[i].length() + 1;
+			}
+
+			else if (temp == 0)
+			{
+				shortword += *iter;
+
+				if (*iter == *(iter + 1))
+				{
+					++temp;
+				}
+				else
+				{
+					shortword += "1";
+					temp = 0;
+				}
+				++iter;
+			}
+
+			
+
+			else
+			{
+				if (*iter == *(iter + 1))
+				{
+					++temp;
+				}
+				else
+				{
+					shortword += to_string(temp + 1);
+					temp = 0;
+				}
+				iter++;
+			}
+
+			
+
+			++k;
+		}
+
+
+		if (shortword.length() >= word[i].length())
+			cout << word[i] << " " << word[i].length() << endl;
+		else
+			cout << shortword << " " << shortword.length() << endl;
+
+	}
+
+
+	delete[] word;
+
+	return 0;
+}
+
