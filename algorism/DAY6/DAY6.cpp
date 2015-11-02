8989141a106af5dc0974398f31877b743e28586b
Vegemil <euna501@gmail.com>
2015-11-02 17:55:18 +0900

    Day6

---
+++ algorism/DAY6/DAY6.cpp
@@ -0,0 +1,15 @@
+// DAY6.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+//문자열 두 개를 입력으로 받아 그중 하나가 다른 하나의 순열인지 판별하는 메서드를 작성하라
+//순열 : 순서대로
+
+#include "stdafx.h"
+#include <string>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+
+	return 0;
+}
+
