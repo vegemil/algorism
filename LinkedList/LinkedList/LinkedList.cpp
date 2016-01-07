c222c7f47047ca72c4dbeacaea12f4e9e583f98b
Vegemil <euna501@gmail.com>
2016-01-08 02:27:08 +0900

    단방향 Linked List
    노드 삽입, 삭제

--- LinkedList/LinkedList/LinkedList.cpp
+++ LinkedList/LinkedList/LinkedList.cpp
@@ -3,9 +3,112 @@
 
 #include "stdafx.h"
 
+struct Node
+{
+	int data;
+	Node* next;
+};
+
+struct List
+{
+	Node *head;
+	Node *tail;
+	Node *current;
+
+	int count;
+};
+
+void NodeInit(List *list)
+{
+	list->head = NULL;
+	list->current = NULL;
+	list->tail = NULL;
+
+	list->count = 0;
+}
+
+void NodeInsert(List *list, int data)
+{
+	Node *newNode = new Node();
+	newNode->data = data;
+	newNode->next = NULL;
+
+	if (list->head == NULL)
+	{
+		list->head = newNode;
+	}
+	else
+	{
+		list->tail->next = newNode;
+	}
+
+	list->tail = newNode;
+	list->count++;
+}
+
+Node* CurFirst(List *list)
+{
+	if (list->head == NULL)
+	{
+		return false;
+	}
+
+	list->current = list->head;
+	return list->current;
+}
+
+Node* CurNext(List *list)
+{
+	if (list->current->next == NULL)
+	{
+		return false;
+	}
+
+	list->current = list->current->next;
+	return list->current;
+}
+
+void NodeRemove(List *list)
+{
+
+	Node* temp = list->current->next;
+
+	delete(list->current);
+
+	list->current = temp;
+	list->count--;
+}
 
 int main()
 {
-    return 0;
-}
+	List list;
+	
+	NodeInit(&list);
 
+	for (int i = 0; i < 4; i++)
+		NodeInsert(&list, i);
+
+	if (CurFirst(&list))
+	{
+		std::cout<< list.current->data << std::endl;
+
+		while (CurNext(&list))
+		{
+			std::cout << list.current->data << std::endl;
+		}
+	}
+
+	std::cout << list.count << std::endl;
+
+	if (CurFirst(&list))
+	{
+		std::cout << list.current->data << " 삭제" <<std::endl;
+		NodeRemove(&list);
+
+		for (int i = 0; i < list.count;)
+		{
+			std::cout << list.current->data << " 삭제" << std::endl;
+			NodeRemove(&list);
+		}
+	}
+}
